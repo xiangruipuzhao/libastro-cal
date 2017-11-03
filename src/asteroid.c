539a03d58d9bbdbb5ba5fffb34023888e7e9b435
faraco <skelic3@gmail.com>
2017-11-04 00:21:31 +0800

    - use double type for better precision (64 bit)

--- src/asteroid.c
+++ src/asteroid.c
@@ -1,1 +1,7 @@
 #include "asteroid.h"
+
+double ac_asteroid_apparent_diameter(double true_diameter, double asteroid_earth_dist)
+{
+    return 1.3788 * true_diameter / asteroid_earth_dist;
+}
+
--- src/asteroid.h
+++ src/asteroid.h
@@ -14,7 +14,7 @@
  * @param albedo Reflective power of the asteroid
  * @return diameter of the asteroid in meters
  */
-float ac_asteroid_diameter(float abs_mag, float albedo);
+double ac_asteroid_diameter(double abs_mag, double albedo);
 
 /**
  * @brief Calculate the apparent diameter of an asteroid
@@ -22,6 +22,6 @@ float ac_asteroid_diameter(float abs_mag, float albedo);
  * @param asteroid_earth_dist Asteroid to Earth distance in Astronomical Unit (AU)
  * @return apparent diameter of the asteroid in meters
  */
-float ac_asteroid_apparent_diameter(float true_diameter, float asteroid_earth_dist);
+double ac_asteroid_apparent_diameter(double true_diameter, double asteroid_earth_dist);
 
 #endif
